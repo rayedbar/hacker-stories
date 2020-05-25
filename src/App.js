1948857dfc63d9fc9f8739301dbdd86bb5436258
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-05-25 14:03:10 +0600

    React Asynchronous Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import './App.css';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const getAsyncStories = () =>
+  new Promise(resolve =>
+    setTimeout(() =>
+      resolve({ data: { stories: initialStories } }),
+      1000
+    )
+  );
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -8,38 +35,25 @@ const useSemiPersistentState = (key, initialState) => {
 
   React.useEffect(() => {
     localStorage.setItem(key, value)
-  }, [value, key])
+  }, [value, key]);
 
-  return [value, setValue]
+  return [value, setValue];
 }
 
 const App = () => {
-  const initialStories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
-  const [stories, setStories] = React.useState(initialStories)
+  const [stories, setStories] = React.useState([]);
 
-  const handleSearch = event => setSearchTerm(event.target.value)
-  const handleRemoveStory = item => setStories(stories.filter(story => story.objectID !== item.objectID))
+  React.useEffect(() => {
+    getAsyncStories().then(result =>
+      setStories(result.data.stories)
+    )
+  }, []);
 
-  const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
+  const handleSearch = event => setSearchTerm(event.target.value);
+  const handleRemoveStory = item => setStories(stories.filter(story => story.objectID !== item.objectID));
+
+  const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <>
