0a4585fbbae33a40b154031e4aef85c3f07269f9
Rayed Bin Wahed <rayed.bin.wahed@g.bracu.ac.bd>
2020-05-25 13:13:32 +0600

    Inline handler in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value)
+  }, [value, key])
+
+  return [value, setValue]
+}
+
 const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -22,13 +34,12 @@ const App = () => {
   ];
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const [stories, setStories] = React.useState(initialStories)
 
-  const handleSearch = event =>
-    setSearchTerm(event.target.value)
+  const handleSearch = event => setSearchTerm(event.target.value)
+  const handleRemoveStory = item => setStories(stories.filter(story => story.objectID !== item.objectID))
 
-  const searchedStories = stories.filter(story =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()))
 
   return (
     <>
@@ -39,21 +50,9 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-      <List stories={searchedStories} />
+      <List stories={searchedStories} onRemoveItem={handleRemoveStory} />
     </>
   );
-}
-
-const useSemiPersistentState = (key, initialState) => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem(key, value)
-  }, [value, key])
-
-  return [value, setValue]
 }
 
 const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
@@ -74,19 +73,24 @@ const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, ch
   )
 }
 
-const List = ({ stories }) =>
+const List = ({ stories, onRemoveItem }) =>
   <ul>
-    {stories.map(({ objectID, ...item }) => <Item key={objectID} {...item} />)}
+    {stories.map(item =>
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+    )}
   </ul>
 
-const Item = ({ title, url, author, num_comments, points }) =>
+const Item = ({ item, onRemoveItem }) =>
   <li>
     <span>
-      <a href={url}>{title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>&nbsp;
-    <span>{author}</span>&nbsp;
-    <span>{num_comments}</span>&nbsp;
-    <span>{points}</span>
+    <span>{item.author}</span>&nbsp;
+    <span>{item.num_comments}</span>&nbsp;
+    <span>{item.points}</span>&nbsp;
+    <span>
+      <button type='button' onClick={() => onRemoveItem(item)}>Dismiss</button>
+    </span>
   </li>
 
 export default App;
